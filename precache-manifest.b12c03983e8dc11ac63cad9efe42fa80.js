b6149c618c1aa4d8bb30c87fdab9b5d28f0a536b
maxime2023 <maximedizier06500@gmail.com>
2020-10-11 18:51:58 +0200

    Updates

---
+++ precache-manifest.b12c03983e8dc11ac63cad9efe42fa80.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3af3d6c687329446a3439f701fc021",
+    "url": "/Kair/index.html"
+  },
+  {
+    "revision": "8b049ef9a35e9f30ee43",
+    "url": "/Kair/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "60ee78bbfafe751acc8d",
+    "url": "/Kair/static/css/main.44e3556c.chunk.css"
+  },
+  {
+    "revision": "8b049ef9a35e9f30ee43",
+    "url": "/Kair/static/js/2.da5a7799.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Kair/static/js/2.da5a7799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ee78bbfafe751acc8d",
+    "url": "/Kair/static/js/main.c7b53cf3.chunk.js"
+  },
+  {
+    "revision": "727d19e5304244bb155c",
+    "url": "/Kair/static/js/runtime-main.616d8f3b.js"
+  },
+  {
+    "revision": "04de1dfc9b815ae8b4610267f201ec55",
+    "url": "/Kair/static/media/fbb.04de1dfc.png"
+  },
+  {
+    "revision": "bd8fd174f141d92fdcec00d172ced3de",
+    "url": "/Kair/static/media/fill.bd8fd174.png"
+  },
+  {
+    "revision": "61214c7a1cb8ab5bc22c5ffcb733e1b6",
+    "url": "/Kair/static/media/filtres.61214c7a.png"
+  },
+  {
+    "revision": "b6dee88006c0c3922f22bf6a42a9e84a",
+    "url": "/Kair/static/media/footer.b6dee880.png"
+  },
+  {
+    "revision": "74238a7b4809785a9d61d1d958194a4f",
+    "url": "/Kair/static/media/housse.74238a7b.png"
+  },
+  {
+    "revision": "4336984f01c91fd4ee306d5b53f9e538",
+    "url": "/Kair/static/media/kair logo.4336984f.png"
+  },
+  {
+    "revision": "df587466173d641bfd265effa8c49dc5",
+    "url": "/Kair/static/media/logo page.df587466.png"
+  },
+  {
+    "revision": "8d50d121c432ee612e1c7cce1bead671",
+    "url": "/Kair/static/media/masssk.8d50d121.png"
+  },
+  {
+    "revision": "56922c69f7629e8c251e9f3d89be7aa6",
+    "url": "/Kair/static/media/p1.56922c69.png"
+  },
+  {
+    "revision": "f103de99602e8f2dfaa88b0b09c06745",
+    "url": "/Kair/static/media/p2.f103de99.png"
+  },
+  {
+    "revision": "677c727627c554c8bb55672cc7f58c28",
+    "url": "/Kair/static/media/p3.677c7276.png"
+  },
+  {
+    "revision": "18e7198f952c8d46e03d86b8ddc1ac82",
+    "url": "/Kair/static/media/p4.18e7198f.png"
+  },
+  {
+    "revision": "477cca0d677d3b7ebb03923684437bc9",
+    "url": "/Kair/static/media/p5.477cca0d.png"
+  },
+  {
+    "revision": "8b3bf15338189a1052f2c05763df6b7a",
+    "url": "/Kair/static/media/p6.8b3bf153.png"
+  },
+  {
+    "revision": "32aa41c66b78d1b6cfaa5d8397309053",
+    "url": "/Kair/static/media/p7.32aa41c6.png"
+  },
+  {
+    "revision": "95b66b6477310edf7c6a11dddcd30e6d",
+    "url": "/Kair/static/media/p8.95b66b64.png"
+  },
+  {
+    "revision": "25da6d64a03391586f5a46605b095aef",
+    "url": "/Kair/static/media/p9.25da6d64.png"
+  },
+  {
+    "revision": "34a9b1eca1c6f713550105516deb64a6",
+    "url": "/Kair/static/media/sac.34a9b1ec.png"
+  },
+  {
+    "revision": "a1d2d86465563c5b4a2a4258f3d67a8d",
+    "url": "/Kair/static/media/ship.a1d2d864.png"
+  },
+  {
+    "revision": "eb391f65456d1ddab002efc4538ba364",
+    "url": "/Kair/static/media/trailer.eb391f65.mp4"
+  },
+  {
+    "revision": "0c584877ea1a1ccad245e145867b7360",
+    "url": "/Kair/static/media/val2.0c584877.png"
+  },
+  {
+    "revision": "0be93c2c26cbac89796ad36ff14396d6",
+    "url": "/Kair/static/media/val4.0be93c2c.png"
+  },
+  {
+    "revision": "b2eae545969807d64191172bed346511",
+    "url": "/Kair/static/media/zeuuuub.b2eae545.png"
+  }
+]);
